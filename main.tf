a6404cf31c4ea56891101009e4f3a25f959d60d1
Nefilime <59085787+Nefilime@users.noreply.github.com>
2021-05-28 10:53:25 +0300

    w

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ variable "resource_group_name" {
 }
 
 resource "azurerm_resource_group" "rg" {
-    name     = "var.resource_group_name" {
+    name     = "test" {
 
     }"
     location = "westus2"
