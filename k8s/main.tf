a60b730c88c2fca50d2ffdb8278906541a97c0ac
Vladimir <6074442@gmail.com>
2021-06-30 11:56:19 +0300

    add providers block to main.tf

--- k8s/main.tf
+++ k8s/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.61.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
 resource "azurerm_resource_group" "aks" {
   name     = "AKS"
   location = "East US 2"
