44333e58e068890c323e3fdf04c55cc5a21aa269
Vladimir <6074442@gmail.com>
2021-07-01 11:05:25 +0300

    add sensitive = true to output

--- k8s/main.tf
+++ k8s/main.tf
@@ -47,8 +47,10 @@ resource "azurerm_kubernetes_cluster" "aks01" {
 
 output "client_certificate" {
   value = azurerm_kubernetes_cluster.aks01.kube_config.0.client_certificate
+  sensitive = true
 }
 
 output "kube_config" {
   value = azurerm_kubernetes_cluster.aks01.kube_config_raw
+  sensitive = true
 }
